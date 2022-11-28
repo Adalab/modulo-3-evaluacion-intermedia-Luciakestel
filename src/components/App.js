889385254b915691d5f7fa379b18e38693172c8c
Luciakestel <luciakestelpires@gmail.com>
2022-11-28 08:42:29 +0100

    add templete react

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import '../styles/App.scss';
+
+function App() {
+
+  //VARIABLES ESTADO
+  //USEEFFECT
+  //FUNCIONES HANDLER
+  //FUNCIONES Y VARIABLES QUE AYUDEN A RENDERIZAR EL HTML
+  //HTML EN EL RETURN
+
+  return (
+    <div className="App">
+     Rellename! 
+    </div>
+
+  );
+}
+
+export default App;
